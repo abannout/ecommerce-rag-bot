534416678697ffe565d288c3b8089c91e020c95e
abannout <92031956+abannout@users.noreply.github.com>
2025-07-20 01:56:26 +0200

    Update supabase.ts

--- src/db/supabase.ts
+++ src/db/supabase.ts
@@ -5,7 +5,6 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-console.log(supabaseUrl + " hehehe " + supabaseAnonKey)
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('❌ Supabase environment variables not loaded.');
 }
