82ecb05bee16eef1f2f07a10da8a98970a8dfef2
abannout <ahmadbannout.ab@gmail.com>
2025-07-20 01:48:41 +0200

    fixed files layout

--- src/lib/embed.ts
+++ src/lib/embed.ts
@@ -1,12 +1,17 @@
 async function embedQuery(query: string) {
     const EMBED_API = process.env.EMBED_API!;
-
+    console.log(EMBED_API)
     const res = await fetch(EMBED_API, {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${process.env.HF_API_KEY}`
+        },
         body: JSON.stringify({ text: query }),
     });
+
     const json = await res.json();
     return json.embedding;
 }
+
 export default embedQuery;
